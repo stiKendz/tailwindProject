5f6c18c841b036733ea35c5f7294eb8bcaeb9168
kendzSharaga <toguedriver@gmail.com>
2024-10-24 14:43:27 +0300

    Доработал страницу регистрации, и тыкал новые фичи tailwind-а

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
   theme: {
     extend: {
 		height: {
-			
+			'1080%75': '810px'
 		},
     fontFamily: {
       'Arima': 'Arima-VariableFont'
+    },
+    width: {
+      '80%': '80%',
+      '2xl': '1440px'
+    },
+    borderWidth: {
+      '20': '20px'
     }
 	 },
   },
